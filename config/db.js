9defee3cc80c31b51d85d37a76efc6fd2240ffdb
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-25 11:15:53 +0500

    connect db

--- config/db.js
+++ config/db.js
@@ -2,31 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    if (mongoose.connection.readyState === 1) {
-      console.log("MongoDB already connected");
-      return mongoose.connection;
-    }
-
     const connection = await mongoose.connect(
-      process.env.MONGO_URI,
-      {
-        serverSelectionTimeoutMS: 60000,
-        connectTimeoutMS: 60000,
-        socketTimeoutMS: 60000,
-
-        // Query buffering timeout
-        bufferTimeoutMS: 60000,
-      }
+      process.env.MONGO_URI
     );
 
     console.log(
       `MongoDB Connected: ${connection.connection.host}`
     );
-
-    return connection;
   } catch (error) {
     console.error("MongoDB Error:", error.message);
-    throw error;
+    process.exit(1);
   }
 };
 
