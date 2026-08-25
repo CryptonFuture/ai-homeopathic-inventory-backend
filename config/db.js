8da7fe1bba93eba9e95a64186adea8baa5899853
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-25 10:37:32 +0500

    cache

--- config/db.js
+++ config/db.js
@@ -1,18 +1,42 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  try {
-    const connection = await mongoose.connect(
-      process.env.MONGO_URI
-    );
+const MONGODB_URI = process.env.MONGODB_URI;
 
-    console.log(
-      `MongoDB Connected: ${connection.connection.host}`
-    );
-  } catch (error) {
-    console.error("MongoDB Error:", error.message);
-    process.exit(1);
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+// Global is used here to maintain a cached connection across hot reloads in development.
+// This prevents connections growing exponentially during API Route usage.
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
 
-export default connectDB;
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
