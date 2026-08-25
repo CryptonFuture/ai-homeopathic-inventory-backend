dfa7fc95d52e44f8890270bfc5a70bf3f6690262
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-25 10:44:21 +0500

    fixed

--- config/db.js
+++ config/db.js
@@ -1,42 +1,18 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const options = {
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 10000,
-    };
-
-    cached.promise = mongoose
-      .connect(process.env.MONGODB_URI, options)
-      .then((mongoose) => {
-        console.log("MongoDB Connected");
-        return mongoose;
-      });
-  }
-
+const connectDB = async () => {
   try {
-    cached.conn = await cached.promise;
+    const connection = await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      `MongoDB Connected: ${connection.connection.host}`
+    );
   } catch (error) {
-    cached.promise = null;
-    console.error("MongoDB Error:", error);
-    throw error;
+    console.error("MongoDB Error:", error.message);
+    process.exit(1);
   }
+};
 
-  return cached.conn;
-}
-
-module.exports = connectDB;
+export default connectDB;
