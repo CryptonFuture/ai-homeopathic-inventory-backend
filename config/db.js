6935644226e4ecc97285a2f4a5b1643ce3e587b1
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-25 10:28:37 +0500

    config

--- config/db.js
+++ config/db.js
@@ -1,24 +1,18 @@
-const mongoose = require("mongoose");
-
-let isConnected = false;
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-  if (isConnected) {
-    console.log("MongoDB already connected");
-    return;
-  }
-
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const connection = await mongoose.connect(
+      process.env.MONGO_URI
+    );
 
-    isConnected = db.connections[0].readyState;
-
-    console.log("MongoDB Connected Successfully");
+    console.log(
+      `MongoDB Connected: ${connection.connection.host}`
+    );
   } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
-
-    throw error;
+    console.error("MongoDB Error:", error.message);
+    process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
